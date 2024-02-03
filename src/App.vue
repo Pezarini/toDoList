aa72e7f60b4ef1e3a3e242f764cfbad91c22f49d
Pedrokelvin <pedrokelvin@ibm.com>
2024-02-03 16:20:01 -0300

    feat: progress bar componente was created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <div id="app"></div>
+	<div id="app">
+		<ProgressBar />
+	</div>
 </template>
 
 <script>
+import ProgressBar from '@/components/ProgressBar.vue'
 
 export default {
-  name: 'App',
+	name: 'App',
+	components: {
+		ProgressBar,
+	},
 }
 </script>
 
 <style>
-  #app {
-
-  }
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		width: 100%;
+	}
 </style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="containerProgress">
+	<h1>Tasks</h1>
+	<div class="barProgress">0%</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+	.containerProgress {
+		display: flex;
+		justify-content: center;
+		align-items: center; 
+		flex-direction: column;
+		width: 100%;
+	}
+
+	.barProgress {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 2rem;
+		width: 50%;  
+		border: 2px solid rgb(0, 0, 0);
+	}
+</style>
