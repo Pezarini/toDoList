2c12da9ac6c97df59c0b15ae73bfed8003797e94
Pedrokelvin <pedrokelvin@ibm.com>
2024-02-05 19:11:12 -0300

    feat: card tasks component was created

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,23 @@
 	<div id="app">
 		<ProgressBar />
 		<AddTasks />
+		<div class="cardTasks">
+			<CardTask />
+		</div>
 	</div>
 </template>
 
 <script>
 import ProgressBar from '@/components/ProgressBar.vue'
 import AddTasks from '@/components/AddTasks.vue'
+import CardTask from '@/components/CardTask.vue'
 
 export default {
 	name: 'App',
 	components: {
 		ProgressBar,
-		AddTasks
+		AddTasks,
+		CardTask
 	},
 }
 </script>
@@ -27,5 +32,11 @@ export default {
 		color: #2c3e50;
 		margin-top: 60px;
 		width: 100%;
+		height: auto;
+	}
+	.cardTasks {
+		width: 100%;
+		display: flex;
+		justify-content: center;
 	}
 </style>
---
+++ src/components/CardTask.vue
@@ -0,0 +1,34 @@
+<template>
+
+	<div class="containerCardTasks">
+		<div class="contentCard">
+			<h2>Task</h2>
+		</div>
+	</div>
+
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+	.containerCardTasks {
+		margin-top: 60px;
+		width: 60%;
+		height: auto;
+		display: flex;
+		flex-wrap: wrap;
+
+	}
+	.contentCard {
+		margin: 10px;
+		border: 2px solid rgb(6, 3, 3);
+		border-radius: 5px ;
+		width: 30%;
+	}
+
+</style>
