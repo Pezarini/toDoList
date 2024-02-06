68b2f5d1d0d491954b7c163bff28126de2279da9
Pedrokelvin <pedrokelvin@ibm.com>
2024-02-06 19:17:58 -0300

    feat: button to remove task

--- src/components/CardTask.vue
+++ src/components/CardTask.vue
@@ -1,46 +1,64 @@
 <template>
-	<h3 v-if="task.length === 0">Day without tasks :)</h3>
+	<h3 v-if="task.length === 0" id="withoutTasks">Day without tasks :)</h3>
 	<div v-else class="containerCardTasks">
-		<div class="contentCard" v-for="taskCard in task" :key="taskCard">
-			<h2>{{taskCard}}</h2>
+		<div 
+			class="contentCard" 
+			v-for="(taskCard, index) in task" 
+			:key="index" >
+			<h2>{{ taskCard }}</h2>
+			<button class="buttonToRemove" @click="removeTask(index)">X</button>
 		</div>
 	</div>
 
 </template>
 
 <script>
-import connectComponents from '@/connectComponents';
+	import connectComponents from '@/connectComponents';
 
 export default {
 	data() {
 		return {
-			task: []
-		}
+			task: [],
+		};
 	},
 	created() {
 		connectComponents.$on('userWrittenTask', (task) => {
 			this.task.push(task);
 		});
+	},
+	methods: {
+		removeTask(index) {
+			this.task.splice(index, 1);
+		},
 	}
-	
-}
-
+};
 </script>
 
 <style>
-	.containerCardTasks {
+	#withoutTasks {
 		margin-top: 60px;
-		width: 60%;
-		height: auto;
-		display: flex;
-		flex-wrap: wrap;
-
 	}
+
+	.containerCardTasks {
+	margin-top: 60px;
+	width: 60%;
+	height: auto;
+	display: flex;
+	flex-wrap: wrap;
+	}
+
 	.contentCard {
-		margin: 10px;
-		border: 2px solid rgb(6, 3, 3);
-		border-radius: 5px ;
-		width: 30%;
+	position: relative;
+	margin: 10px;
+	border: 2px solid rgb(6, 3, 3);
+	border-radius: 5px;
+	width: 30%;
 	}
 
+	.buttonToRemove {
+	position: absolute;
+	top: 5px;
+	right: 5px;
+	cursor: pointer;
+	}
 </style>
