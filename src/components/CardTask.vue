fc2adc166141ff78a2add19fe0643b420b66be58
Pedrokelvin <pedrokelvin@ibm.com>
2024-02-14 19:34:11 -0300

    chore: make the completed task affect the progress bar

--- src/components/CardTask.vue
+++ src/components/CardTask.vue
@@ -1,11 +1,13 @@
 <template>
 	<h3 v-if="task.length === 0" id="withoutTasks">Day without tasks :)</h3>
 	<div v-else class="containerCardTasks">
-		<div 
-			class="contentCard" 
-			v-for="(taskCard, index) in task" 
-			:key="index" >
-			<h2>{{ taskCard }}</h2>
+		<div
+			class="contentCard"
+			v-for="(taskCard, index) in task"
+			:key="index"
+			:class="{'taskReady': task[index].conditional}"
+			@click="doneTask(index)">
+			<h2>{{ taskCard.text }}</h2>
 			<button class="buttonToRemove" @click="removeTask(index)">X</button>
 		</div>
 	</div>
@@ -19,16 +21,28 @@ export default {
 	data() {
 		return {
 			task: [],
+			tasksFinished: 0,
 		};
 	},
 	created() {
-		connectComponents.$on('userWrittenTask', (task) => {
-			this.task.push(task);
+		connectComponents.$on('taskInput', (task) => {
+			this.task.push({text: task, conditional: false});
 		});
 	},
 	methods: {
 		removeTask(index) {
 			this.task.splice(index, 1);
+			this.task[index].conditional = false;
+			connectComponents.$emit('howManyTasks', this.task.length);
+		},
+		doneTask(index) {
+			this.task[index].conditional = !this.task[index].conditional;
+	
+			if (!this.task[index].conditional) {
+				this.tasksFinished -= 2;
+			}
+			this.tasksFinished += 1;
+			connectComponents.$emit('howManyTasks', {tasks: this.task.length, tasksFinished: this.tasksFinished});
 		},
 	}
 };
@@ -40,25 +54,30 @@ export default {
 	}
 
 	.containerCardTasks {
-	margin-top: 60px;
-	width: 60%;
-	height: auto;
-	display: flex;
-	flex-wrap: wrap;
+		margin-top: 60px;
+		width: 60%;
+		height: auto;
+		display: flex;
+		flex-wrap: wrap;
 	}
 
 	.contentCard {
-	position: relative;
-	margin: 10px;
-	border: 2px solid rgb(6, 3, 3);
-	border-radius: 5px;
-	width: 30%;
+		position: relative;
+		margin: 10px;
+		border-radius: 5px;
+		background: #dedede;
+		box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1); 
+		width: 30%;
 	}
 
 	.buttonToRemove {
-	position: absolute;
-	top: 5px;
-	right: 5px;
-	cursor: pointer;
+		position: absolute;
+		top: 5px;
+		right: 5px;
+		cursor: pointer;
+	}
+
+	.taskReady {
+		background-color: rgb(27, 235, 27);
 	}
 </style>
--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="containerProgress">
 	<h1>Tasks</h1>
-	<div class="barProgress">0%</div>
+	<div class="contentProgress">
+		<div 
+			class="barProgress"
+			:class="{'withoutBorder': this.calculatePercentage === 0}"
+			:style="{ width: this.calculatePercentage + '%' }">
+			<span>{{ this.calculatePercentage + '%' }}</span>
+		</div>
+	</div>
   </div>
 </template>
 
 <script>
-export default {
+import connectComponents from '@/connectComponents';
 
+export default {
+	data() {
+		return {
+			lengthTask: 0,
+			lengthTaskFinished: 0,
+		}
+	},
+	computed: {
+		calculatePercentage() {
+			return 100 / this.lengthTask === Infinity ? 0 : Math.floor((this.lengthTaskFinished / this.lengthTask) * 100);
+		}
+	},
+	created() {
+		connectComponents.$on('howManyTasks', (lengthTasks) => {
+			this.lengthTask = lengthTasks.tasks;
+			this.lengthTaskFinished = lengthTasks.tasksFinished;
+		});
+	},
 }
 </script>
 
@@ -20,12 +45,26 @@ export default {
 		width: 100%;
 	}
 
+	.contentProgress{
+		width: 50%;  
+		height: 2rem;
+		box-shadow: 7px 7px 10px rgba(0, 0, 0, 0.1);
+	}
+
 	.barProgress {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		height: 2rem;
-		width: 50%;  
-		border: 2px solid rgb(0, 0, 0);
+		background: rgb(71, 250, 0) ;
+	}
+
+	.barProgress span {
+		position: absolute;
+		right: 46.5rem
+	}
+
+	.withoutBorder{
+		border: none;
 	}
 </style>
